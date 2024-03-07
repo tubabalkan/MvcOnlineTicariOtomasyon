61193f58ddf6569f6f86c5a9f3f4436e57abdcfc
tuba balkan <tubabalkan165@gmail.com>
2024-03-08 00:41:01 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class KategoriController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Kategoris.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateKategori(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteKategori(int id)
+        {
+            var ktg = c.Kategoris.Find(id);
+            c.Kategoris.Remove(ktg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = c.Kategoris.Find(id);
+
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var ktgr = c.Kategoris.Find(k.KategoriID);
+            ktgr.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CarilerID { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CarilerAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(30)]
+        public string CarilerSoyad { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(15)]
+        public string CarilerSehir { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CarilerMail { get; set; }
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+    }
+}

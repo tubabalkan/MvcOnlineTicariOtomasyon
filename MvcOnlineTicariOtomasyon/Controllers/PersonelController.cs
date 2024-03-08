30cf2383432510f801db1c7159bfb22e0b5e044a
tuba balkan <tubabalkan165@gmail.com>
2024-03-09 00:20:15 +0300

    personelcontroller eklendi ve işlemler yapıldı

---
+++ MvcOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Personels.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanId.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAd,
+                                               Value = x.DepartmanId.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var prs = c.Personels.Find(id);
+            return View("PersonelGetir", prs);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+
+            var prsn = c.Personels.Find(p.PersonelID);
+            prsn.PersonelAd = p.PersonelAd;
+            prsn.PersonelSoyad = p.PersonelSoyad;
+            prsn.PersonelGorsel = p.PersonelGorsel;
+            prsn.Departmanid = p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}

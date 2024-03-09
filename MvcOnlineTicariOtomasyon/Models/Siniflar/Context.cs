ab371c258bf5d65a33bcb974c71ae5e310e7cfda
tuba balkan <tubabalkan165@gmail.com>
2024-03-10 00:15:06 +0300

    urundetay controller oluşturuldu

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Context.cs
@@ -17,6 +17,9 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public DbSet<Personel> Personels { get; set; }
         public DbSet<SatisHareket> SatisHarekets { get; set; }
         public DbSet<Urun> Uruns { get; set; }
+        public DbSet<Detay> Detays{ get; set; }
+       
+    
 
     }
 }
